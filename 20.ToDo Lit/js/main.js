13675a368a371e807c7ee7661e3553206ae03a2b
unknown <attentionaddress@gmail.com>
2023-05-05 02:00:38 +0500

    lighter is working but not del

--- 20.ToDo Lit/js/main.js
+++ 20.ToDo Lit/js/main.js
@@ -1,35 +1,29 @@
 const list = document.querySelector('.list')
 list.insertAdjacentHTML('beforebegin',
     `<div class='header'>
-<p>Tasks</p>
-<div class='header-add'>
-<input type='date' class='list-add-date'> 
-<input type='text' class='list-add-text' placeholder='Add note'>
-<button id='btn-note'>Add Note</button>
-<label for="imp">Important</label>
-<input type='checkbox' id='important'>
+<p class="header-logo">Tasks</p>
+<div class='header-inputs'>
+<input type='date' class='header-inputs-date'> 
+<input type='text' class='header-inputs-text' placeholder='Add note'>
+<button id='header-inputs-btn'>Add Note</button>
+<label for="header-inputs-important" class="header-inputs-label">Important</label>
+<input type='checkbox' id='header-inputs-important'>
 </div>  
 <div class='header-buttons'>
 <button class='list-logOut'>LogOut</button>
 <button class='list-profile'>My profile</button>
 </div>
-
 </div>`
 )
+
 list.insertAdjacentHTML('afterend',
     `<div class='menu'>
 <input type='radio' name='radio-in' id='All'>
-<input type='radio' name='radio-in' id='Process'>
+<label for="All" class='menu-lab-all'><i class="fa-regular fa-rectangle-list"></i>All</label>
 
-    <label for="All" class='lab-all'>
-    <i class="fa-regular fa-rectangle-list"></i>
-    All
-    </label>
-    <label for="Process" class='lab-pro'>
-    <i class="fa-regular fa-calendar-check"></i>
-    Process
-    </label>
-    </div>`
+<input type='radio' name='radio-in' id='Process'>
+<label for="Process" class='menu-lab-pro'><i class="fa-regular fa-calendar-check"></i>Process</label>
+</div>`
 )
 list.innerHTML = `   
 <div class="list-radio">
@@ -38,42 +32,65 @@ list.innerHTML = `
 </div>
 </div>
 `
-const important = document.querySelector('#important')
-const btn = document.querySelector('#btn-note')
-const date = document.querySelector('.list-add-date')
-const note = document.querySelector('.list-add-text')
+
+
+
+const important = document.querySelector('#header-inputs-important')
+const btn = document.querySelector('#header-inputs-btn')
+const date = document.querySelector('.header-inputs-date')
+const note = document.querySelector('.header-inputs-text')
+const listNote = document.querySelector('.list-note')
 const time = new Date()
-let ul
+let noteNew
+
+
 btn.addEventListener('click', () => {
-    const listNote = document.querySelector('.list-note')
+
     if (date.value != "" && note.value != "") {
         const newNote = `
-            <ul class="note-list">
-            <li >${note.value}</li>
-            <input type='radio' id='exercise'>
+<ul class="list-note-new">
+            <li class="note-new-text">${note.value}</li>
             <li>${date.value}</li>
-                <li>${time.getHours()}:${time.getMinutes()}</li>
-                <div>
-                <input type="checkbox" id='edit'>
-                <label for="edit">
-                <i class="fa-solid fa-pen-to-square" style="color: #ffd43b;"></i>
-                </label>
-                <input type="checkbox" id='delete'>
-                <label for='delete'>
-                <i class="fa-solid fa-trash" style="color:rgb(29,105,171)"></i>
-                </label>
-                </div>
-            </ul>
-        `
+            <li>${time.getHours()}:${time.getMinutes()}</li>
+
+    <div>
+<input type="checkbox" id='edit'>
+<label for="edit"><i class="fa-solid fa-pen-to-square" style="color: #ffd43b;"></i></label>
+<input type="checkbox" id='delete' class='delete'>
+<label for='delete'><i class="fa-solid fa-trash" style="color:rgb(29,105,171)"></i></label>
+    </div>
+</ul>`
+
         listNote.insertAdjacentHTML('afterbegin', newNote)
-        ul = document.querySelector('.note-list')
+        noteNew = document.querySelector('.list-note-new')
 
 
-        if (imp.checked == true) {
-            ul.insertAdjacentHTML('afterbegin', `<span style='background:red' class='span-dot'></span>`)
+        if (important.checked == true) {
+            noteNew.insertAdjacentHTML('afterbegin', `<span style='background:red' class='span-dot'></span>`)
         } else {
-            ul.insertAdjacentHTML('afterbegin', `<span style='background:orange' class='span-dot'></span>`)
+            noteNew.insertAdjacentHTML('afterbegin', `<span style='background:orange' class='span-dot'></span>`)
         }
+        const noteText = document.querySelectorAll(".note-new-text")
+        noteText.forEach(color => {
+            color.addEventListener('click', (element) => {
+                if (element.target.classList.contains('note-new-text')) {
+                    const listSpan = element.target.parentElement.querySelector(".span-dot")
+                    listSpan.style.background = 'green'
+                }
+            })
+        });
+
+        const del = document.querySelectorAll(".delete")
+        del.forEach(remove => {
+            remove.addEventListener('click', (element) => {
+                if (element.target.closest('.delete')) {
+                    element.target.closest('.list-note-new').remove()
+                }
+
+            })
+        })
+
+
     } else if (date.value == "" && note.value != "") {
         alert('Select date')
     } else if (date.value != "" && note.value == "") {
@@ -81,17 +98,10 @@ btn.addEventListener('click', () => {
     } else {
         alert('select date and write note!')
     }
+
 })
 
 
-const sp = document.querySelector('.span-dot')
-const exercise = document.querySelector('#exer')
-exercise.addEventListener('click', () => {
-    sp.style.background = 'green'
-})
 
-const deleteNote = document.querySelector('#delete')
-deleteNote.addEventListener('click', (even) => {
-    even.classList.toggle("ul");
-})
+
 
